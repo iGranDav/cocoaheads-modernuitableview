313114f3007fb6744da0cc3565e8c9cb834b29f7
David Bonnet <david@harpp.io>
2018-03-05 22:14:10 +0100

    Add swiftgen configuration

---
+++ CookEat/CookEat/Code/Constants/Strings.swift
@@ -0,0 +1,22 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// swiftlint:disable identifier_name line_length type_body_length
+enum L10n {
+  /// CookEat
+  static let appname = L10n.tr("Localizable", "appname")
+}
+// swiftlint:enable identifier_name line_length type_body_length
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
